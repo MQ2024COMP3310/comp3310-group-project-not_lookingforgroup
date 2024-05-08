52fcb9bf445c3591a12095434079225b11636f98
Adrian Kane <egg_on_stilts@hotmail.com>
2024-05-09 03:41:12 +1000

    Added a tool to get a local virtual environment set up with consistency

---
+++ setupVenv.bat
@@ -0,0 +1,21 @@
+@echo ####################
+@echo This will set up a local virtual python environment at ./env
+@echo press enter to continue or Ctrl + C to break.
+@echo ####################
+@pause
+
+python -m venv %cd%/env
+
+@echo Activating environment
+
+call env\Scripts\activate
+
+@echo now in Environment
+pip install -r requirements.txt
+@echo ####################
+@echo Finished setup, dropping into command prompt.
+@echo INFO: This prompt is running in the virtual environment.
+@echo ####################
+@pause
+
+cmd -k
